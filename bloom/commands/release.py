90285270afd5b7c0f983fec4460fa20c669c038c
Felix Exner <feex@universal-robots.com>
2025-07-29 14:08:49 +0200

    Use plain github URLs without the token
    This way we can get until pushing the PR content to the rosdistro fork
    without a token with workflow permissions.

--- bloom/commands/release.py
+++ bloom/commands/release.py
@@ -742,8 +742,8 @@ Increasing version of package(s) in repository `{repository}` to `{version}`:
                 from subprocess import check_call
                 check_call(cmd, shell=True)
             # Use the oauth token to clone
-            rosdistro_url = 'https://{gh.token}:x-oauth-basic@github.com/{base_repo_id}.git'.format(**locals())
-            fork_template = 'https://{gh.token}:x-oauth-basic@github.com/{head_org}/{head_repo}.git'
+            rosdistro_url = 'https://github.com/{base_repo_id}.git'.format(**locals())
+            fork_template = 'https://github.com/{head_org}/{head_repo}.git'
             rosdistro_fork_url = fork_template.format(**locals())
             _my_run("mkdir -p {base_info[repo]}".format(**locals()))
             with change_directory(base_info['repo']):
